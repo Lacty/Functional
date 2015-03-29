e6b8914ae24bcf2e8a1165aac388025c71ec05f6
U-PS14B22\vantan <akira206@gmail.com>
2015-03-29 16:49:52 +0900

    引数付き関数ポインタ

--- Functional/main.cpp
+++ Functional/main.cpp
@@ -4,20 +4,32 @@
 // 関数ポインタ＆関数オブジェクト
 
 
-// ふつうの関数で同じことができるかやってみた
-int *result();
-int hoge(){
-  return 10;
+// int型引数付き関数ポインタ
+int(*result)(int, int);
+int add(int x, int y){
+  return x + y;
+}
+int multi(int x, int y){
+  return x * y;
 }
 
 int main(){
-  *result() = hoge();
-  std::cout << result() << std::endl;
+  int temp;
 
-  // コンパイルエラーでた
-  // ダメな感じかな？
+  {
+    result = add;
+    temp   = result(2, 5);
 
-  // そもそもコードが汚いね
+    std::cout << temp << std::endl;
+    // 出力結果：7
+  }
 
+  {
+    result = multi;
+    temp   = result(3, 4);
+
+    std::cout << temp << std::endl;
+    // 出力結果：12
+  }
   return 0;
 }
